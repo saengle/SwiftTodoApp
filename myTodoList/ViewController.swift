cb2a1f83d341e6ce36c5d970717a94513aeb88b0
saengle <zohv817@gmail.com>
2024-03-22 13:35:35 +0900

    Add: Plus Button

--- myTodoList/ViewController.swift
+++ myTodoList/ViewController.swift
@@ -8,13 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+        private let button: UIButton = {
+            let button = UIButton()
+//            button.setTitle("아무말이없어서?", for: .normal)
+//            button.backgroundColor = .systemBlue
+//            button.layer.cornerRadius = 7
+            button.setImage(UIImage(systemName: "plus.circle"), for: .normal)
+            return button
+        }()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.setupUI()
+        
+        self.button.addTarget(self, action: #selector(didTapAddButton), for: .touchUpInside)
     }
-
-
+    private func setupUI() {
+        self.view.addSubview(button)
+        self.button.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor, constant: -25),
+            button.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 15),
+//            button.widthAnchor.constraint(equalToConstant: 300),
+//            button.heightAnchor.constraint(equalToConstant: 300),
+        ])
+    }
+    
+    @objc func didTapAddButton() {
+        print("버튼 누름")
+    }
 }
 
 struct todo {
@@ -26,3 +51,7 @@ struct todo {
 //    var password: String
 //    let dueDate: String   기한설정 스트링 ?
 }
+
+//#Preview {
+//  ViewController()
+//}
